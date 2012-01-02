dcff1ee514353b3644ee5b1356ea97872090258a
unknown <Yier Jin@YierJin-laptop.(none)>
2012-01-01 23:27:42 -0600

    structural model

--- DES_cycle_nat_des.v
+++ DES_cycle_nat_des.v
@@ -1,3 +1,13 @@
+(* In order to support fully sensitivity tag assignments, I revamp the whole Coq circuit 
+representative architecture with the first step is redefine the bus architecture:
+*** Definition bus := nat -> nat.
+With the first nat denoting time, the second nat denoting the position of the bus in the 
+whole sensitivity tag list.
+    We also add a new sensitivity tag list to represent all internal signals' sensitivities 
+associated with time.
+*** Definition sen_list := nat -> list nat.
+*)
+
 
 (* As the second step towards information flow secrecy tracking, I will try to 
 dynamically trace the information flow tags for all internal/input/output signals.
@@ -23,229 +33,118 @@ Require Import Bool Arith List MinMax.
 
 Search le.
 
-Eval compute in max 3 5.
 
 
 
-Inductive value := lo | hi.
-Inductive sensitivity := protect | normal. 
-(* I use 'protect' instead of 'secure' to make it clear the underlying signal
-with 'protect' tag needs protection. *)
 
 Section des.
 
-Definition bus_value := list value.
-(* Definition bus := nat -> (bus_value * sensitivity). *)
-Definition bus := nat -> (bus_value * nat). (* the sensitivity tag is now a number indicating sensitivity level. *)
+Definition bus := nat -> nat. (* the sensitivity tag is now a number stored
+in sensitivity tag list. *)
 
-
-Definition VDD : bus := fun t : nat => (hi::nil, O).
-Definition GND : bus := fun t : nat => (lo::nil, O).
+Definition sen_list := nat -> list nat.
 
 Definition sliceA (b : bus) (p1 p2 : nat) : bus :=
-  fun t : nat => (firstn (p2-p1+1) (skipn (p1-1) (fst (b t))), snd (b t)).
-
-Definition v := lo::lo::hi::hi::lo::nil.
-
-Definition b := fun t:nat => (v, 2).
-Definition b2 := fun t:nat => (v, O).
-
-Eval compute in sliceA b2 1 3.
-
-
+  fun t : nat => (b t).
 
 Definition sliceD (b : bus) (p1 p2 : nat) : bus :=
-  fun t : nat => match (b t) with
-                 | (bv, n) => (rev (firstn (p1-p2+1) (skipn p2 (rev bv))), n)
-                 end.
+  fun t : nat => (b t).
+(* The slicing operation only keeps the bus sensitivity list position. 
+It's reasonable and necessary to assume that any part of the bus shares the same sensitivity level
+as the whole bus. And any changes to the part of the bus will be reflected on the whole bus 
+sensitivity. *)
 
 Notation "b [ m , n ] " := (sliceD b m n) (at level 50, left associativity).
 Notation "b @ [ m , n ] " := (sliceA b m n) (at level 50, left associativity).
 
-Eval compute in b[3,1].
-
-
+(* No bus length operation is required in this new model.
 Definition bus_length (b : bus) :=
   fun t : nat => length (fst (b t)).
+*)
 
-
-
-Definition not (a : value) : value :=
-  match a with 
-  | lo => hi
-  | hi => lo
-  end.
-
-
-(* BUS/TAG OPERATIONS. Ubiquitous calculation. *)
-Fixpoint bv_bit_not (a : bus_value) {struct a} : bus_value :=
-  match a with
-  | nil => nil
-  | la :: a' => (not la) :: (bv_bit_not a')
-  end.
 
 Definition bus_bit_not (b : bus) : bus :=
-  fun t : nat => (bv_bit_not (fst (b t)), snd (b t)).
+  fun t : nat => (b t).
 
 Definition uoptag (a : nat) : nat := a.
 Definition boptag (a b : nat) : nat := max a b.
+
+Fixpoint max_list (l : list nat) : nat :=
+  match l with
+  | nil => O
+  | a :: rl => max a (max_list rl)
+  end.
+
+Definition list_update (sl : list nat) (pos : nat) (a : nat) : list nat :=
+  (firstn pos sl) ++ (a::nil) ++ (skipn (pos+1) sl).
+
+  Fixpoint firstn (n:nat)(l:sen_list) : sen_list :=
+    match n with
+      | 0 => nil
+      | S n => match l with
+                 | nil => nil
+                 | a::l => a::(firstn n l)
+               end
+    end.
+
+  Fixpoint skipn (n:nat)(l:list A) : list A :=
+    match n with
+      | 0 => l
+      | S n => match l with
+                 | nil => nil
+                 | a::l => skipn n l
+               end
+    end.
+
+Definition sen_update (sl : sen_list) (n : nat) (new_sen : nat) : sen_list :=
+  (firstn n sl) ++ (new_sen::nil) ++ (skipn (n+1) sl).
+soif
+Definition test := 1::3::5::3::11::6::nil.
+Eval compute in sen_update test 0 666.
+Eval compute in sen_update test 1 666.
+Eval compute in list_update test 2 666.
+Eval compute in list_update test 666 3.
+Eval compute in list_update test 666 4.
+Eval compute in list_update test 666 5.
+Eval compute in list_update test 666 6.
+Eval compute in list_update test 666 7.
+Eval compute in max_list test.
+
 
 Definition lowertag (a : nat) : nat := pred a.
 
 Definition a_number := 3.
 Eval compute in lowertag a_number.
 
-Definition xor (a b : value) := 
-  match a with
-  | lo => match b with
-          | lo => lo
-          | hi => hi end
-  | hi => match b with
-          | lo => hi
-          | hi => lo end
-  end.
-
-Fixpoint bv_bit_xor (a b : bus_value) {struct a} :=
-  match a with
-  | nil => nil
-  | la :: a' => match b with
-                | nil => nil
-                | lb :: b' => (xor la lb) :: (bv_bit_xor a' b')
-                end
-  end.   (* Again, only consider the case that bus widths are the same. *)
-
 Definition bus_bit_xor (a b : bus) : bus :=
-  fun t:nat => (bv_bit_xor (fst (a t)) (fst (b t)), boptag (snd (a t)) (snd (b t))).
-
-Definition and (a b : value) : value :=
-  match a with
-  | lo => lo
-  | hi => match b with
-          | lo => lo
-          | hi => hi
-          end
-  end.
-
-Fixpoint bv_bit_and (a b : bus_value) {struct a} : bus_value :=
-  match a with
-  | nil => nil
-  | la :: a' => match b with
-                | nil => nil
-                | lb :: b' => (and la lb) :: (bv_bit_and a' b')
-                end
-  end.
+  fun t:nat => boptag (a t) (b t).
 
 Definition bus_bit_and (a b : bus) : bus :=
-  fun t:nat => (bv_bit_and (fst (a t)) (fst (b t)), boptag (snd (a t)) (snd (b t))).
-
-Eval compute in bus_bit_and b2 b2.
-
-Definition or (a b : value) :=
-  match a with
-  | lo => match b with
-          | lo => lo
-          | hi => hi end
-  | hi => hi
-  end.
-
-Fixpoint bv_bit_or (a b : bus_value) {struct a} : bus_value :=
-  match a with
-  | nil => nil
-  | la :: a' => match b with
-                | nil => nil
-                | lb :: b' => (or la lb) :: (bv_bit_or a' b')
-                end
-  end.
-
+  fun t:nat => boptag (a t) (b t)(b t).
 
 Definition bus_bit_or (a b : bus) : bus :=
-  fun t:nat => (bv_bit_or (fst (a t)) (fst (b t)), boptag (snd (a t)) (snd (b t))).
+  fun t:nat => boptag (a t) (b t).
 
 Definition bus_app (a b : bus) : bus :=
-  fun t:nat => ((fst (a t)) ++ (fst (b t)), boptag (snd (a t)) (snd (b t))).
+  fun t:nat => boptag (a t) (b t).
 
 
-(* Some Comparison Operations. *)
-Fixpoint bv_eq (a b : bus_value) {struct a} : value :=
-  match a with
-  | nil => hi
-  | la :: a' => match b with
-                | nil => hi
-                | lb :: b' => match (la, lb) with
-                              | (lo, lo) => bv_eq a' b'
-                              | (lo, hi) => lo
-                              | (hi, lo) => lo
-                              | (hi, hi) => bv_eq a' b'
-                              end
-                 end
-  end.
+(* No operations are required in the new model. *)
 
-Definition sen_eq (a b : nat) : value :=
-  match (beq_nat a b) with
-  | true => hi
-  | false => lo
-  end.
+Definition sen_eq (a b : nat) : value := beq_nat a b.
 
+Definition bus_eq (a b : bus) (t : nat) : bus :=
+  fun t:nat => boptag (a t) (b t).
 
-Definition bus_eq (a b : bus) (t : nat) : value :=
-  and (bv_eq (fst (a t)) (fst (b t))) (sen_eq (snd (a t)) (snd (b t))).
+Definition bus_lt (a b : bus) (t : nat) : bus :=
+  fun t:nat => boptag (a t) (b t).
 
+Definition bus_gt (a b : bus) (t : nat) : bus :=
+  fun t:nat => boptag (a t) (b t).
 
-Fixpoint bv_lt (a b : bus_value) {struct a} :=
-  match a with
-  | nil => lo
-  | la :: a' => match b with
-                | nil => lo
-                | lb :: b' => match (la, lb) with
-                              | (lo, lo) => bv_lt a' b'
-                              | (lo, hi) => hi
-                              | (hi, lo) => lo
-                              | (hi, hi) => bv_lt a' b'
-                              end
-                end
-  end.
+Definition bus_eq_0 (a : bus) (t : nat) : bus := a.
 
-Definition bus_lt (a b : bus) (t : nat) : value :=
-  bv_lt (fst (a t)) (fst (b t)).    
-(* Again, we assume that the tag of comparison buses are the same. *)
-
-Fixpoint bv_gt (a b : bus_value) {struct a} :=
-  match a with
-  | nil => lo
-  | la :: a' => match b with
-                | nil => lo
-                | lb :: b' => match (la, lb) with
-                              | (lo, lo) => bv_gt a' b'
-                              | (lo, hi) => lo
-                              | (hi, lo) => hi
-                              | (hi, hi) => bv_gt a' b'
-                              end
-                 end
-  end.
-
-Definition bus_gt (a b : bus) (t : nat) : value :=
-  bv_gt (fst (a t)) (fst (b t)).  
-(* Again, we assume that the tag of comparison buses are the same. *)
-
-Fixpoint bv_eq_0 (a : bus_value) {struct a} : value :=
-  match a with
-  | hi :: lt => lo
-  | lo :: lt => bv_eq_0 lt
-  | nil => hi
-  end.
-
-Definition bus_eq_0 (a : bus) (t : nat) : value :=
-  bv_eq_0 (fst (a t)).   
-(* Again, the sensitivity of the bus does not matter. *)
-(* The comparison with 0 is used for most if-else branches. *)
-
-Lemma bv_eq_refl : forall (t : nat) (a : bus_value), (bv_eq a a) = hi.
-Proof.
-  intros. unfold bv_eq.
-  induction a. trivial.
-  rewrite IHa. destruct a. trivial. trivial.
-Qed.
-
+(*
 Lemma bus_eq_refl : forall (t : nat) (a : bus), (bus_eq a a t) = hi.
 Proof.
   intros. unfold bus_eq. unfold sen_eq. SearchAbout beq_nat. rewrite <- beq_nat_refl.
@@ -259,7 +158,7 @@ Qed.
 
 Definition l_test : list nat := 3::4::6::nil.
 Eval compute in nth 0 l_test.
-
+*)
 
 Fixpoint sen_list_merge (sl1 sl2 : list nat) : list nat :=
   match sl1 with
@@ -270,12 +169,10 @@ Fixpoint sen_list_merge (sl1 sl2 : list nat) : list nat :=
 		  end
   end.
   
-  
-(* Finishing basic operation definition. Note that we modified the notation of the sensitivity
-tag from binary (secure|normal) to a multi-level natural number.
-With the new sensitivity tag, we can represent more sophisticated circuit architecture with 
-pipelines. *)
-
+(* The expression is the smallest element of the Coq circuit representative.
+The evaluation operation defines/calculate one signal nat value, the senstivitity tag, of the
+whole expression. 
+The later assignment statements will put the nat value back to the sensitivity list. *)  
 Inductive expr :=
   | econv : bus_value -> expr
   | econb : bus -> expr
@@ -290,78 +187,33 @@ Inductive expr :=
   | gt : expr -> expr -> expr
   | case3 : expr -> expr -> expr -> expr -> expr -> expr -> expr -> expr -> expr.
 
-Fixpoint eval (e : expr) (t : nat) {struct e} : bus_value*nat :=
-  match e with
-  | econv v => (v, O)
-  | econb b => b t
-  | eand ex1 ex2 => (bv_bit_and (fst (eval ex1 t)) (fst (eval ex2 t)), boptag (snd (eval ex1 t)) (snd (eval ex2 t)))
-  | eor ex1 ex2 => (bv_bit_or (fst (eval ex1 t)) (fst (eval ex2 t)), boptag (snd (eval ex1 t)) (snd (eval ex2 t)))
-  | exor ex1 ex2 => (bv_bit_xor (fst (eval ex1 t)) (fst (eval ex2 t)), boptag (snd (eval ex1 t)) (snd (eval ex2 t)))
-  | enot ex => (bv_bit_not (fst (eval ex t)), uoptag (snd (eval ex t)))
-  | cond cex ex1 ex2 => match (bv_eq_0 (fst (eval cex t))) with
-                        | hi => eval ex1 t
-                        | lo => eval ex2 t end
-  | perm ex => (fst (eval ex t), lowertag (snd (eval ex t)))
-  | sbox b => (b t)
-  | eq ex1 ex2 => match (bv_eq (fst (eval ex1 t)) (fst (eval ex2 t))) with
-                  | hi => (hi :: nil, normal)
-                  | lo => (lo :: nil, normal) end
-  | lt ex1 ex2 => match (bv_lt (fst (eval ex1 t)) (fst (eval ex2 t))) with
-                  | hi => (hi :: nil, normal)
-                  | lo => (lo :: nil, normal) end
-  | gt ex1 ex2 => match (bv_gt (fst (eval ex1 t)) (fst (eval ex2 t))) with
-                  | hi => (hi :: nil, normal)
-                  | lo => (lo :: nil, normal) end
-  | case3 sel e1 e2 e3 e4 e5 e6 e7 e8 =>     
-                  match fst (eval sel t) with
-                  | lo::lo::lo::nil => eval e1 t
-                  | lo::lo::hi::nil => eval e2 t
-                  | lo::hi::lo::nil => eval e3 t
-                  | lo::hi::hi::nil => eval e4 t
-                  | hi::lo::lo::nil => eval e5 t
-                  | hi::lo::hi::nil => eval e6 t
-                  | hi::hi::lo::nil => eval e7 t
-                  | hi::hi::hi::nil => eval e8 t
-                  | _ => eval e1 t
-                  end
-  end.
-
-Fixpoint eval_sen (e : expr) (t : nat) {struct e} : nat :=  (* only evalute the sensitivity tag
-								and return a nat value. *)
+Fixpoint sen_eval (e : expr) (t : nat) (sl : sen_list){struct e} : nat :=
   match e with
   | econv v => O
-  | econb b => snd (b t)
-  | eand ex1 ex2 => boptag (snd (eval ex1 t)) (snd (eval ex2 t))
-  | eor ex1 ex2 => boptag (snd (eval ex1 t)) (snd (eval ex2 t))
-  | exor ex1 ex2 => boptag (snd (eval ex1 t)) (snd (eval ex2 t))
-  | enot ex => uoptag (snd (eval ex t))
-  | cond cex ex1 ex2 => match (bv_eq_0 (fst (eval cex t))) with
-                        | hi => eval_sen ex1 t
-                        | lo => eval_sen ex2 t end
-  | perm ex => lowertag (snd (eval ex t))
-  | sbox b => snd (b t)
-  | eq ex1 ex2 => match (bv_eq (fst (eval ex1 t)) (fst (eval ex2 t))) with
-                  | hi => O
-                  | lo => O end
-  | lt ex1 ex2 => match (bv_lt (fst (eval ex1 t)) (fst (eval ex2 t))) with
-                  | hi => O
-                  | lo => O end
-  | gt ex1 ex2 => match (bv_gt (fst (eval ex1 t)) (fst (eval ex2 t))) with
-                  | hi => O
-                  | lo => O end
-  | case3 sel e1 e2 e3 e4 e5 e6 e7 e8 =>     
-                  match fst (eval sel t) with
-                  | lo::lo::lo::nil => eval_sen e1 t
-                  | lo::lo::hi::nil => eval_sen e2 t
-                  | lo::hi::lo::nil => eval_sen e3 t
-                  | lo::hi::hi::nil => eval_sen e4 t
-                  | hi::lo::lo::nil => eval_sen e5 t
-                  | hi::lo::hi::nil => eval_sen e6 t
-                  | hi::hi::lo::nil => eval_sen e7 t
-                  | hi::hi::hi::nil => eval_sen e8 t
-                  | _ => eval_sen e1 t
-                  end
+  | econb b => nth (b t) (sl t)
+  | eand ex1 ex2 => boptag (sen_eval ex1 t sl) (sen_eval ex2 t sl)
+  | eor ex1 ex2 => boptag (sen_eval ex1 t sl) (sen_eval ex2 t sl)
+  | exor ex1 ex2 => boptag (sen_eval ex1 t sl) (sen_eval ex2 t sl)
+  | enot ex => sen_eval ex t sl
+  | cond cex ex1 ex2 => boptag (sen_eval ex1 t sl) (sen_eval ex2 t sl)
+  | perm ex => lowertag (sen_eval ex t sl)
+  | sbox b => nth (b t) (sl t)
+  | eq ex1 ex2 => boptag (sen_eval ex1 t sl) (sen_eval ex2 t sl)
+  | lt ex1 ex2 => boptag (sen_eval ex1 t sl) (sen_eval ex2 t sl)
+  | gt ex1 ex2 => boptag (sen_eval ex1 t sl) (sen_eval ex2 t sl)
+  | case3 sel e1 e2 e3 e4 e5 e6 e7 e8 =>
+		max_list 
+                  (sen_eval sel t sl) ::
+                  (sen_eval e1 t sl) ::
+                  (sen_eval e2 t sl) ::
+                  (sen_eval e3 t sl) ::
+                  (sen_eval e4 t sl) ::
+                  (sen_eval e5 t sl) ::
+                  (sen_eval e6 t sl) ::
+                  (sen_eval e7 t sl) ::
+                  (sen_eval e8 t sl) :: nil
   end.
+
 
 Inductive signal :=
   | outb : bus -> signal
@@ -385,18 +237,6 @@ Inductive code :=
 
 Notation " c1 ; c2 " := (codepile c1 c2) (at level 50, left associativity).
 
-Definition expr_sen (e : expr) (t : nat) : nat :=
-  snd (eval e t).
-
-Definition bus_sen (b : bus) (t : nat) : nat :=
-  snd (eval (econb b) t).
-
-Axiom sub_bus_sen : forall (t : nat) (p1 p2 : nat) (b : bus), 
-  snd (b t) = snd ((b @ [p1, p2]) t).
-(* ensure that any part of the bus shares the same sensitivity of the 
-whole bus. *)
-
-
 
 (* a.k.a. RTL code file *)
 
@@ -405,18 +245,22 @@ Variables desIn key : bus.
 Variables decrypt roundSel clk : bus.
 Variables K_sub IP FP L R Xin Lout Rout out : bus.
 
+Variable des_sen_list : sen_list.
+
 Axiom secret_key : forall (t : nat), bus_sen key t = 1.
 Axiom secret_desIn : forall (t : nat), bus_sen desIn t = 1.
 Axiom secret_K_sub : forall (t : nat), bus_sen K_sub t = 1.
 
-Axiom normal_init_K_sub: bus_sen K_sub 0 = 0.
-Axiom normal_init_IP: bus_sen IP 0 = 0.
-Axiom normal_init_FP: bus_sen FP 0 = 0.
-Axiom normal_init_L: bus_sen L 0 = 0.
-Axiom normal_init_R: bus_sen R 0 = 0.
-Axiom normal_init_Xin: bus_sen Xin 0 = 0.
-Axiom normal_init_Lout: bus_sen Lout 0 = 0.
-Axiom normal_init_Rout: bus_sen Rout 0 = 0.
+Axiom normal_init_K_sub: bus_sen K_sub 0 = 0.  (* #0 *)
+Axiom normal_init_IP: bus_sen IP 0 = 0.        (* #1 *)
+Axiom normal_init_FP: bus_sen FP 0 = 0.        (* #2 *)
+Axiom normal_init_L: bus_sen L 0 = 0.          (* #3 *)
+Axiom normal_init_R: bus_sen R 0 = 0.          (* #4 *)
+Axiom normal_init_Xin: bus_sen Xin 0 = 0.      (* #5 *)
+Axiom normal_init_Lout: bus_sen Lout 0 = 0.    (* #6 *)
+Axiom normal_init_Rout: bus_sen Rout 0 = 0.    (* #7 *)
+Axiom normal_init_out: bus_sen out 0 = 0.      (* #8 *)
+Axiom normal_init_desOut: bus_sen desOut 0 = 0.(* #9 *)
 
 Definition des_signals : signal :=
   outb desOut &   	(* #9 *)
@@ -434,6 +278,24 @@ Definition des_signals : signal :=
   wireb Lout &		(* #6 *)
   wireb Rout &		(* #7 *)
   wireb out.		(* #8 *)
+
+
+Fixpoint upd_signals_sen (c : code) (t : nat) (sl : sen_list) : sen_list :=
+  match c with
+  | assign_ex b ex => sen_update sl (b t) (sen_eval ex t sl)
+  | assign_b b1 b2 => bus_sen b2 t
+  | assign_case3 b ex => expr_sen ex t
+  | nonblock_assign_ex b ex => expr_sen ex t  (* added in DES_frame_des.v. *)
+  | nonblock_assign_b b1 b2 => bus_sen b2 t  (* added in DES_frame_des.v. *)
+  | module_inst2in bout b1 b2 => normal    (* added in DES_frame_des.v to deal with module instantiation. *)
+  | module_inst3in bout b1 b2 b3 => normal  (* added in DES_frame_des.v to deal with module instantiation. *)
+  | codepile c1 c2 => boptag (chk_code_sen c1 t) (chk_code_sen c2 t)
+
+
+
+  | codepile c1 c2 => sen_list_merge (upd_signals_sen c1) (upd_signals_sen c2)
+  | 
+
 
 Definition line1 : code :=
   assign_ex Lout (cond (eq (econb roundSel) (econv (lo::lo::lo::lo::nil))) (econb (IP @ [33, 64])) (econb R)).
@@ -479,10 +341,6 @@ Definition line10 : code :=
 
 
 
-Fixpoint upd_signals_sen (c : code) (sl : list nat) : list nat :=
-  match c with
-  | codepile c1 c2 => sen_list_merge (upd_signals_sen c1) (upd_signals_sen c2)
-  | 
 
 
 
