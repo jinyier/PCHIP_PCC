c8582826daaa63a42dddc3fcde952fd6b58059e2
jinyier <jinyier@gmail.com>
2012-01-12 16:34:43 -0600

    loop

--- AES_cycle_key.v
+++ AES_cycle_key.v
@@ -187,6 +187,15 @@ Fixpoint chk_code_sen (n:nat) (c:code) (sl : code_sen) : code_sen :=
   end.
 
 
+(* ****************************************************** *)
+
+(* inputs *)
+
+
+
+
+
+
 (********************************************************* *)
 
 (* inputs *)
@@ -504,45 +513,24 @@ Definition aes_sen_stable : code_sen :=
 1 :: 1 :: 1 :: 1 :: 1 :: 1 :: 1 :: 1 :: 1 :: 1 :: 
 1 :: 0 :: 0 :: 0 :: 0 :: nil.
 
-Lemma stable_code_sen_upd :  upd_code_sen aes aes_sen_stable = aes_sen_stable.
+Lemma stable_code_sen :  upd_code_sen aes aes_sen_stable = aes_sen_stable.
 Proof.
   intros. reflexivity.
 Qed.
 
-Lemma stable_code_sen_chk : forall t:nat, chk_code_sen t aes aes_sen_stable = aes_sen_stable.
-Proof.
-  intros. 
-  induction t. reflexivity.
-  unfold chk_code_sen. fold chk_code_sen. rewrite stable_code_sen_upd.
-  apply IHt.
-Qed.
-
-Lemma stable_state : forall t:nat, t = 5 -> chk_code_sen t aes aes_sen_initial = aes_sen_stable.
-Proof.
-  intros.
-  rewrite H. reflexivity.
-Qed.
-
-Lemma stable_state_cal : (upd_code_sen aes
-     (upd_code_sen aes
-        (upd_code_sen aes
-           (upd_code_sen aes (upd_code_sen aes aes_sen_initial))))) = aes_sen_stable.
-Proof. 
-  intros. reflexivity.
-Qed.
 
 
 Theorem no_leaking : forall t : nat, t > 5 -> 
   (chk_code_sen t aes aes_sen_initial) = aes_sen_stable.
 Proof. 
-  intros. induction H. reflexivity.  
-  unfold chk_code_sen. fold chk_code_sen. 
-  induction H. reflexivity. unfold chk_code_sen. fold chk_code_sen.  
-  induction H. reflexivity. unfold chk_code_sen. fold chk_code_sen. 
-  induction H. reflexivity. unfold chk_code_sen. fold chk_code_sen. 
-  induction H. reflexivity. unfold chk_code_sen. fold chk_code_sen.
-  rewrite stable_state_cal. apply stable_code_sen_chk.
+  intros. induction H. reflexivity.  ???
+  unfold chk_code_sen. simpl. simpl. auto. 
+  destruct m.
+
+rewrite stable_code_sen. simpl. apply IHle.
 Qed.
+
+
 
 
 
